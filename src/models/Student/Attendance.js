f333950cfd97c4062f6f5f0b80c39f98ef3f5305
Mondalgithub <shovanmondal2004@gmail.com>
2025-02-09 11:07:45 +0530

    Attendance Data handling in bulk

--- src/models/Student/Attendance.js
+++ src/models/Student/Attendance.js
@@ -1,33 +1,17 @@
 import mongoose from "mongoose";
 
-const attendanceSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.ObjectId,
-      ref: "User",
-      required: true,
+const attendanceSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  semester: { type: Number, required: true },
+  month: { type: Number, required: true },
+  subjects: [
+    {
+      subjectName: String,
+      attendedClasses: Number,
+      totalClasses: Number,
     },
-    semester: {
-      type: Number,
-      required: true,
-    },
-    month: {
-      type: String,
-      required: true,
-    },
-
-    subjects: [
-      {
-        subjectCode: String,
-        subjectName: String,
-        attendedClasses: Number,
-        totalClasses: Number,
-      },
-    ],
-    overallAttendance: Number,
-  },
-  { timestamps: true }
-);
-
+  ],
+  overallAttendance: Number,
+});
 const Attendance = mongoose.model("Attendance", attendanceSchema);
 export default Attendance;

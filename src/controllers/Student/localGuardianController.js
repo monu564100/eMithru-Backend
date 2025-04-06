7a425f43f56e86417e1d0249a447c8fa29c8365e
MONU KUMAR <144309004+monu564100@users.noreply.github.com>
2025-04-06 16:16:46 +0530

    Update localGuardianController.js

--- src/controllers/Student/localGuardianController.js
+++ src/controllers/Student/localGuardianController.js
@@ -1,127 +1,32 @@
-// import { LocalGuardian } from "../../models/Student/LocalGuardian.js";
-// import catchAsync from "../../utils/catchAsync.js";
-// import AppError from "../../utils/appError.js";
-
-// // Create a new local guardian profile
-// export const createLocalGuardian = catchAsync(async (req, res, next) => {
-//   const localGuardianData = req.body;
-
-//   const localGuardian = await LocalGuardian.create(localGuardianData);
-  
-//   res.status(201).json({
-//     status: "success",
-//     data: {
-//       localGuardian,
-//     },
-//   });
-// });
-
-// // Get local guardian by ID
-// export const getLocalGuardianById = catchAsync(async (req, res, next) => {
-//   const { id } = req.params;
-
-//   const localGuardian = await LocalGuardian.findById(id);
-
-//   if (!localGuardian) {
-//     return next(new AppError("Local Guardian not found", 404));
-//   }
-
-//   res.status(200).json({
-//     status: "success",
-//     data: {
-//       localGuardian,
-//     },
-//   });
-// });
-
-// // Get all local guardians
-// export const getAllLocalGuardians = catchAsync(async (req, res, next) => {
-//   const localGuardians = await LocalGuardian.find();
-
-//   res.status(200).json({
-//     status: "success",
-//     data: {
-//       localGuardians,
-//     },
-//   });
-// });
-
-// // Update local guardian by ID
-// export const updateLocalGuardianById = catchAsync(async (req, res, next) => {
-//   const { id } = req.params;
-//   const updateData = req.body;
-
-//   const updatedLocalGuardian = await LocalGuardian.findByIdAndUpdate(id, updateData, {
-//     new: true,
-//     runValidators: true,
-//   });
-
-//   if (!updatedLocalGuardian) {
-//     return next(new AppError("Local Guardian not found", 404));
-//   }
-
-//   res.status(200).json({
-//     status: "success",
-//     data: {
-//       localGuardian: updatedLocalGuardian,
-//     },
-//   });
-// });
-
-// // Delete local guardian by ID
-// export const deleteLocalGuardianById = catchAsync(async (req, res, next) => {
-//   const { id } = req.params;
-
-//   const deletedLocalGuardian = await LocalGuardian.findByIdAndDelete(id);
-
-//   if (!deletedLocalGuardian) {
-//     return next(new AppError("Local Guardian not found", 404));
-//   }
-
-//   res.status(204).json({
-//     status: "success",
-//     data: null,
-//   });
-// });
-
 import { LocalGuardian } from "../../models/Student/LocalGuardian.js";
 import catchAsync from "../../utils/catchAsync.js";
 import AppError from "../../utils/appError.js";
 
 // Create or update a local guardian profile
 export const createOrUpdateLocalGuardian = catchAsync(async (req, res, next) => {
-  const { userId, ...localGuardianData } = req.body; // Extract userId from request body
+  const { userId, ...localGuardianData } = req.body;
 
-  // Check if a Local Guardian profile already exists for the userId
   const existingLocalGuardian = await LocalGuardian.findOne({ userId });
 
   if (existingLocalGuardian) {
-    // If a profile exists, update it
-    Object.assign(existingLocalGuardian, localGuardianData); // Update existing profile with new data
-    await existingLocalGuardian.save(); // Save updated profile
+    Object.assign(existingLocalGuardian, localGuardianData);
+    await existingLocalGuardian.save();
     return res.status(200).json({
       status: "success",
-      data: {
-        localGuardian: existingLocalGuardian,
-      },
+      data: { localGuardian: existingLocalGuardian },
     });
   } else {
-    // If no profile exists, create a new one
     const localGuardian = await LocalGuardian.create({ userId, ...localGuardianData });
-
     res.status(201).json({
       status: "success",
-      data: {
-        localGuardian,
-      },
+      data: { localGuardian },
     });
   }
 });
 
-// Get local guardian by ID
+// Get local guardian by MongoDB ID
 export const getLocalGuardianById = catchAsync(async (req, res, next) => {
   const { id } = req.params;
-
   const localGuardian = await LocalGuardian.findById(id);
 
   if (!localGuardian) {
@@ -130,25 +35,36 @@ export const getLocalGuardianById = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: "success",
-    data: {
-      localGuardian,
-    },
+    data: { localGuardian },
+  });
+});
+
+// Get local guardian by User ID (not Mongo ID)
+export const getLocalGuardianByUserId = catchAsync(async (req, res, next) => {
+  const { userId } = req.params;
+  const localGuardian = await LocalGuardian.findOne({ userId });
+
+  if (!localGuardian) {
+    return next(new AppError("Local Guardian not found", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { localGuardian },
   });
 });
 
 // Get all local guardians
 export const getAllLocalGuardians = catchAsync(async (req, res, next) => {
   const localGuardians = await LocalGuardian.find();
-
   res.status(200).json({
     status: "success",
-    data: {
-      localGuardians,
-    },
+    results: localGuardians.length,
+    data: { localGuardians },
   });
 });
 
-// Update local guardian by ID
+// Update local guardian by MongoDB ID
 export const updateLocalGuardianById = catchAsync(async (req, res, next) => {
   const { id } = req.params;
   const updateData = req.body;
@@ -164,19 +80,17 @@ export const updateLocalGuardianById = catchAsync(async (req, res, next) => {
 
   res.status(200).json({
     status: "success",
-    data: {
-      localGuardian: updatedLocalGuardian,
-    },
+    data: { localGuardian: updatedLocalGuardian },
   });
 });
 
-// Delete local guardian by ID
+// Delete local guardian by MongoDB ID
 export const deleteLocalGuardianById = catchAsync(async (req, res, next) => {
   const { id } = req.params;
 
-  const deletedLocalGuardian = await LocalGuardian.findByIdAndDelete(id);
+  const deletedGuardian = await LocalGuardian.findByIdAndDelete(id);
 
-  if (!deletedLocalGuardian) {
+  if (!deletedGuardian) {
     return next(new AppError("Local Guardian not found", 404));
   }
 

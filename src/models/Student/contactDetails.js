aa3b18426528eb453d138ff9ff1b069744ca3823
MONU KUMAR <144309004+monu564100@users.noreply.github.com>
2025-04-06 16:13:46 +0530

    Update contactDetails.js

--- src/models/Student/contactDetails.js
+++ src/models/Student/contactDetails.js
@@ -1,40 +1,61 @@
-// models/contactDetails.js
-import mongoose from "mongoose" ;
+import mongoose from "mongoose";
 
-const contactDetailsSchema = new mongoose.Schema({
-    line1: {
-        type: String,
-        required: true,
-    },
-    line2: {
-        type: String,
-    },
-    country: {
-        type: String,
-        required: true,
-    },
-    state: {
-        type: String,
-        required: true,
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    district: {
-        type: String,
-        required: true,
-    },
-    taluka: {
-        type: String,
-        required: true,
-    },
-    pinCode: {
-        type: String,
-        required: true,
-    },
-}, { timestamps: true });
+const addressSchema = new mongoose.Schema({
+  line1: {
+    type: String,
+    required: true,
+  },
+  line2: {
+    type: String,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  district: {
+    type: String,
+    required: true,
+  },
+  taluka: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+  },
+});
 
-const ContactDetails = mongoose.model('ContactDetails', contactDetailsSchema);
+const contactDetailsSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User", // assuming you have a User model
+    },
+    currentAddress: {
+      type: addressSchema,
+      required: true,
+    },
+    permanentAddress: {
+      type: addressSchema,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ContactDetails = mongoose.model("ContactDetails", contactDetailsSchema);
 
 export default ContactDetails;
